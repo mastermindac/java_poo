862786ebe26ea3c4a5a082caec8b3a406126d452
pacomastermind <paco@mastermind.ac>
2022-10-17 12:18:01 +0200

    Ejemplo datosBasicos

---
+++ datosbasicos/src/datosbasicos/Principal.java
@@ -0,0 +1,22 @@
+package datosbasicos;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// Variables numericas
+		byte numOrdenadores =2;
+		System.out.println("numero de ordenadores "+numOrdenadores);
+		
+		int edadPaco=47;
+		System.out.println("Edad de Paco "+edadPaco);
+		
+		float temperatura=23.2f;
+		System.out.println("Temperatura "+temperatura);
+		
+		String nombreAcademia="Mastermind";
+		System.out.println(nombreAcademia);
+	
+
+	}
+
+}
